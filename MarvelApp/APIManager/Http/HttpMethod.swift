f178d75d804973b648c3c1b15931d8952a961c67
Juan David Lopera Lopez <juan.lopez@valid.com>
2022-01-24 22:18:19 -0500

    APIManager
    APIManager module finished

---
+++ MarvelApp/APIManager/Http/HttpMethod.swift
@@ -0,0 +1,13 @@
+//
+//  HttpMethod.swift
+//  APIManager
+//
+//  Created by Juan David Lopera Lopez on 24/01/22.
+//
+
+import Foundation
+
+public enum HttpMethod: String {
+    case post = "POST"
+    case get = "GET"
+}
