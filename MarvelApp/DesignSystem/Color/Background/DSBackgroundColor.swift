c18a0593ccd72513118ab959f018399ac573ade7
Juan David Lopera Lopez <juan.lopez@valid.com>
2022-01-24 22:29:12 -0500

    Design System
    Design System base created

---
+++ MarvelApp/DesignSystem/Color/Background/DSBackgroundColor.swift
@@ -0,0 +1,36 @@
+//
+//  DSBackgroundColor.swift
+//  DesignSystem
+//
+//  Created by Juan David Lopera Lopez on 24/01/22.
+//
+
+import UIKit
+
+public enum DSBackgroundColor {
+    case clear
+    case mainScreen
+    case cadetBlue
+    case divider
+    case selectedDivider
+    
+    func getColor() -> UIColor {
+        switch self {
+        case .clear:
+            return UIColor.clear
+        case .mainScreen:
+            let mainBackgroundColorName: String = "MainBackground"
+            return UIColor(named: mainBackgroundColorName, in: bundle, compatibleWith: nil) ?? UIColor.white
+        case .cadetBlue:
+            let cadetBlueColorName: String = "CadetBlue"
+            return UIColor(named: cadetBlueColorName, in: bundle, compatibleWith: nil) ?? UIColor.cyan
+        case .divider:
+            let dividerColorName: String = "Divider"
+            return UIColor(named: dividerColorName, in: bundle, compatibleWith: nil) ?? UIColor.lightGray
+        case .selectedDivider:
+            let selectedDividerColorName: String = "SelectedDivider"
+            return UIColor(named: selectedDividerColorName, in: bundle, compatibleWith: nil) ?? UIColor.black
+        }
+    }
+}
+
